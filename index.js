6d870a0e7e731ce0a58a975f14290c092c53a8e5
Lucas Alves <zombnfl@gmail.com>
2023-02-09 17:21:44 -0300

    Esqueleto dos modelos para pães, blends, queijos e vegetais

---
+++ index.js
@@ -0,0 +1,19 @@
+const models = {
+    paes : [
+        {tipo: "Brioche", preco: "4,50", quantidade: 1}, 
+        {tipo: "Batata", preco: "3,80", quantidade: 1}
+    ],
+    blend: [
+        {tipo: "Bovino", preco: "7,00", quantidade: 1},
+        {tipo: "Suino", preco: "5,00", quantidade: 1}
+    ],
+    queijos: [
+        {tipo: "Suíço", preco: "2,00", quantidade: 1}
+    ], 
+    vegetais: [
+        {tipo: "Alface", preco:"0,50", quantidade: 1}
+    ]
+}
+
+
+
